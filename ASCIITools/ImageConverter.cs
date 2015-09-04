08eec66a06053b4cc34b4790d78854efeb8a7fbb
caleb <caleb.williams@bayoucitysoft.com>
2015-09-04 07:10:42 -0500

    Added color

--- ASCIITools/ImageConverter.cs
+++ ASCIITools/ImageConverter.cs
@@ -42,12 +42,66 @@ namespace ASCIITools
             return message;
         }
 
+        //File.WriteAllLines(path + ".txt", image);
+        //    Image img = new Bitmap((image.Count() + 1) * 10, image.Length * 10);
+        //    Graphics draw = Graphics.FromImage(img);
+        //    SizeF textSize = draw.MeasureString(image[0], new Font(FontFamily.GenericMonospace, 2F));
+        //    draw.Clear(Color.White);
+        //    draw.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+        //    Brush text = new SolidBrush(Color.Black);
+        //    float y = 0;
+        //    Font font = new Font(FontFamily.GenericMonospace, 10.0F, FontStyle.Bold);
+        //    for (int i = 0; i < image.Count(); i++)
+        //        draw.DrawString(image[i], font, text, new PointF(0, y += font.SizeInPoints));
+
+        public static void ConvertColorImage(Image img)
+        {
+            int ratio = img.Width / img.Height;
+            Image toDraw = new Bitmap(img.Width * 10, img.Height * 10);
+            Graphics draw = Graphics.FromImage(toDraw);
+            draw.Clear(Color.White);
+            draw.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+            Font font = new Font(FontFamily.GenericSansSerif, 8F);
+
+            Bitmap bmp = new Bitmap(img);
+            string[] message = new string[bmp.Height];
+            string row = string.Empty;
+            float _y = 0;
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color color = bmp.GetPixel(x, y);
+                    int grey = ConvertToGrayScale(color);
+                    Color colorGreyScale = Color.FromArgb(grey, grey, grey);
+                    string toPaint = ASCIIPixel((int)colorGreyScale.R).ToString();
+
+
+                    Brush text = new SolidBrush(color);
+                    draw.DrawString(toPaint, font, text, new PointF((float)x * 10 , (float)y * 10));
+                    text.Dispose();
+                }
+            }
+
+            draw.Save();
+            draw.Dispose();
+            toDraw.Save(@"C:\color_img.png", ImageFormat.Png);
+            toDraw.Dispose();
+        }
+
+        private static Color Darken(Color color)
+        {
+            float red = color.R;
+            float blue = color.B;
+            float green = color.G;
+
+            return Color.FromArgb(color.A, (int)red, (int)blue, (int)green);
+        }
+
 
         private static char ASCIIPixel(int shade)
         {
             char dot = '.';
-         
-
             if (shade >= 250)
                 return (char)32;    //' '
             else if (shade >= 245)
@@ -68,21 +122,14 @@ namespace ASCIITools
                 return (char)63;    //!
             else if (shade >= 85)
                 return (char)37;    //$
-            else if (shade >= 45)
-                return (char)64;    //8
-            else if (shade >= 35)
+            else if (shade >= 10)
                 return (char)56;    //@
-            else if (shade >= 25)
-                return (char)38;
             else if (shade >= 0)
                 return (char)35;    //#
-
-          
-
             return dot; 
         }
 
-        private static int ConvertToGrayScale(Color color)
+        public static int ConvertToGrayScale(Color color)
         {
             return (color.R / 3) + (color.G / 3) + (color.B / 3);
         }
@@ -99,7 +146,7 @@ namespace ASCIITools
             draw.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
             Brush text = new SolidBrush(Color.Black);
             float y = 0;
-            Font font = new Font(FontFamily.GenericMonospace, 10.0F);
+            Font font = new Font(FontFamily.GenericMonospace, 10.0F, FontStyle.Bold);
             for (int i = 0; i < image.Count(); i++)
                 draw.DrawString(image[i], font, text, new PointF(0, y += font.SizeInPoints));
 
@@ -110,10 +157,9 @@ namespace ASCIITools
             img.Save(@"C:\image.png", ImageFormat.Png);
         }
 
-        internal static void SaveColorImage(string[] message, Image img)
+        internal static void SaveImage(Image img, string path)
         {
-            
-            
+
         }
     }
 }
--- ASCIITools/Program.cs
+++ ASCIITools/Program.cs
@@ -11,13 +11,15 @@ namespace ASCIITools
         static void Main(string[] args)
         {
             Console.WriteLine("Please select a path to gather an image");
-            string path = Console.ReadLine();
+
+            string path = @"C:\Users\Pax Prose\Pictures\" + Console.ReadLine();
 
             var picture = ImageConverter.GetImage(path);
-            string[] message = ImageConverter.ConvertGreyImage(picture);
+            //string[] message = ImageConverter.ConvertGreyImage(picture);
             //string[] message2 = ImageConverter.ConvertColorImage(picture);
+            ImageConverter.ConvertColorImage(picture);
             Console.WriteLine("Saving Image...");
-            ImageConverter.SaveImage(message, path);
+           // ImageConverter.SaveImage(message, path);
             Console.WriteLine("Image Saved!");
 
             Console.ReadLine();
